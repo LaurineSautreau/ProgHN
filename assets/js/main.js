cc8fee8002c677375bd3778e8e62b18d1067f40a
LaurineSautreau <123956962+LaurineSautreau@users.noreply.github.com>
2023-04-06 14:05:10 +0200

    Projet 3/6
    • ajouter la segmentation en lignes au chargement du texte
    • ajouter le nombre de lignes non vides au message affiché lorsqu’un fichier est
    chargé
    • stocker dans une variable globale les lignes du texte
    • à partir de ces lignes et d’un pôle, développer une fonction grep() qui
    affichera les lignes où une expression régulière est reconnue quelque part
    • colorier en rouge les empans de textes reconnus
    • associer cette fonctionnalité au bouton "action 2", à renommer "grep"
    • ajouter des messages d’erreur s’il manque des paramètres nécessaire pour les
    fonctionnalités "dictionnaire" et "grep" (fichier chargé, pôle...)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,6 @@
+let text_tokens = [];
+
+
 window.onload = function() {
     let fileInput = document.getElementById('fileInput');
     let fileDisplayArea = document.getElementById('fileDisplayArea');
@@ -21,42 +24,119 @@ window.onload = function() {
             // dans la zone d'affichage du texte.
             reader.onload = function(e) {
                 fileDisplayArea.innerText = reader.result;
+                segmentation();
+                document.getElementById("logger").innerHTML = '<span class="infolog">Fichier chargé avec succès, ' + text_tokens.length + ' tokens dans le texte.</span>';
             }
 
             // on lit concrètement le fichier.
             // Cette lecture lancera automatiquement la fonction "onload" juste au-dessus.
-            reader.readAsText(file);    
-
-            document.getElementById("logger").innerHTML = '<span class="infolog">Fichier chargé avec succès</span>';
+            reader.readAsText(file);
         } else { // pas un fichier texte : message d'erreur.
             fileDisplayArea.innerText = "";
             document.getElementById("logger").innerHTML = '<span class="errorlog">Type de fichier non supporté !</span>';
         }
     });
 }
-
 // Fonction bouton aide 	
 
-function aide() {	
-	var etat = document.getElementById('texteaide').style.display;
-         var bouton = document.getElementById('aide');
- 
-         if (etat =='none')
-     {
-         document.getElementById('texteaide').style.display='block';
- 
-     }
-     else
-     {
-         document.getElementById('texteaide').style.display='none';
-     }
+function afficheCacheAide() {
+    let aide = document.getElementById("texteaide");
+    let boutonAide = document.getElementById("boutonAide");
+    let display = texteaide.style.display;
+    
+    if (display === "none") {
+        aide.style.display = "block";
+        boutonAide.innerText = "Cacher l'aide";
+    } else {
+        aide.style.display = "none";
+        boutonAide.innerText = "Afficher l'aide";
+    }
 }
 
-// Fonction bouton segmentation 
+// Fonction bouton segmentation
 
-function decoupage(){
-	let text = "a,b,c,d";
-	let myArray = text.split(" ,;’'~|&#@=`-.?!%*$()[]{}_:+«»§\/");
-	var bouton = document.ElementById('decoupage');
+function segmentation() {
+    let text = document.getElementById("fileDisplayArea").innerText;
+    let delim = document.getElementById("delimID").value;
+    let display = document.getElementById("page-analysis");
 
+    let regex_delim = new RegExp(
+        "["
+        + delim
+            .replace("-", "\\-") // le tiret n'est pas à la fin : il faut l'échapper, sinon erreur sur l'expression régulière
+            .replace("[", "\\[").replace("]", "\\]") // à changer sinon regex fautive, exemple : [()[]{}] doit être [()\[\]{}], on doit "échapper" les crochets, sinon on a un symbole ] qui arrive trop tôt.
+        + "\\s" // on ajoute tous les symboles d'espacement (retour à la ligne, etc)
+        + "]+" // on ajoute le + au cas où plusieurs délimiteurs sont présents : évite les tokens vides
+    );
+
+    let tokens_tmp = text.split(regex_delim);
+    text_tokens = tokens_tmp.filter(x => x.trim() != ''); // on s'assure de ne garder que des tokens "non vides"
+
+    // global_var_tokens = tokens; // décommenter pour vérifier l'état des tokens dans la console développeurs sur le navigateur
+    // display.innerHTML = tokens.join(" ");
+	
+	let ligne = text.split("\n");// segmentation en lignes 
+	text_ligne = ligne.filter(x => x.trim() != ''); // nombre de lignes non vides 
+	let autre = text.split(ligne,"\n","g") 	// stocker dans une variable gloable les lignes du texte 
+	messagelignesnonvides ='<span style=" background:red;">Nombre de lignes non vides</span>';
+	document.getElementById("fileDisplayArea").innerHTML = messagelignesnonvides;
+
+}
+
+// Fonction bouton dictionnaire 
+
+function dictionnaire() {
+    let comptes = new Map();
+    let display = document.getElementById("page-analysis");
+
+    for (let token of text_tokens) {
+        comptes.set(token, (comptes.get(token) ?? 0) + 1);
+    }
+    
+    let comptes_liste = Array.from(comptes);
+    comptes_liste = comptes_liste.sort(function(a, b) {
+        // solution attendue
+        return b[1] - a[1]; // tri numérique inversé
+
+
+    });
+
+    let table = document.createElement("table");
+    table.style.margin = "auto";
+    let entete = table.appendChild(document.createElement("tr"));
+    entete.innerHTML = "<th>mot</th><th>compte</th>";
+    
+    for (let [mot, compte] of comptes_liste) {
+        let ligne_element = table.appendChild(document.createElement("tr"));
+        let cellule_mot = ligne_element.appendChild(document.createElement("td"));
+        let cellule_compte = ligne_element.appendChild(document.createElement("td"));
+        cellule_mot.innerHTML = mot;
+        cellule_compte.innerHTML = compte;
+    }
+
+    display.appendChild(table);
+	
+	
+	messagederreur ='<span style=" background:red;">Aucun fichier chargé</span>';
+	document.getElementById("fileDisplayArea").innerHTML = messagederreur;
+  
+}
+
+
+
+// Fonction bouton grep 
+
+function grep() {
+	
+	let text = document.getElementById("fileDisplayArea").innerText;
+    let poleID = document.getElementById("poleID").value;
+    let display = document.getElementById("page-analysis");
+	
+	let ligne = document.getElementById('fileDisplayArea').innerText;
+	let ligne = new RegExp("\\w+", "g");
+
+	
+	
+	messagederreur ='<span style=" background:red;">Aucun pôle chargé</span>';
+	document.getElementById("fileDisplayArea").innerHTML = messagederreur;
 }
